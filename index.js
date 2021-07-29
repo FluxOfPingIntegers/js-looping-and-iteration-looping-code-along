971e92b341447cafd19fd86319abe92fbf8400fa
FluxOfPingIntegers <Ryan.M.Schleck@gmail.com>
2021-07-29 09:11:38 -0400

    commit tree 6ef852c2e8ef5125bf34667416d542a18cc17c3a

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  const answer = [];
+  for (let i = 0; i < names.length; i++) {
+      answer.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  return answer;
+}
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--;
+  }
+}
